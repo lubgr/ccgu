5b29e3b1c1b812ee9be6860b1e3d0abaaee3708d
Lukas Böger <dev@lboeger.de>
2018-06-25 23:12:50 +0200

    rewrite main benchmark control

--- benchmarks/duplicate.cpp
+++ benchmarks/duplicate.cpp
@@ -1,43 +1,52 @@
 
 #include <cassert>
+#include <cstdio>
 #include "timemeasure.h"
-#include <boost/lexical_cast.hpp>
 #include "duplicate.h"
 
-int main(int argc, char **argv)
+void logResult(int id, double ms, unsigned nDuplicates, unsigned nGroups, unsigned nGroupLengths)
 {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << "[number-of-identical-elements]" << std::endl;
+    std::printf("[%u-%u-%u] version %d: %.3f ms\n", nDuplicates, nGroups, nGroupLengths, id, ms);
+}
 
-        exit(1);
-    }
-
-    const auto nIdenticalElements = boost::lexical_cast<unsigned>(argv[1]);
-    const unsigned origSize = 10'000;
-    const auto nGroups = 10;
-    const auto n = 2;
-
-    std::vector<int> vec(nGroups*nIdenticalElements);
+void measureSample(unsigned nDuplicates, unsigned nGroups, unsigned nGroupLengths)
+{
+    std::vector<int> vec;
+    using std::end;
+    using std::begin;
     int value = 0;
 
-    for (unsigned i = 0; i < nGroups; ++i) {
-        ++value;
-
-        std::fill(begin(vec) + i*nIdenticalElements, begin(vec) + i*nIdenticalElements + nIdenticalElements, value);
-    }
+    for (auto i = 0; i < static_cast<int>(nGroups); ++i)
+        vec.insert(cend(vec), nGroupLengths, i + 1);
 
     const auto orig = vec;
 
-    measure("version 1", duplicateInPlace1<vec::value_type>, vec, n);
+    auto ms = measure(duplicateInPlace1<int>, vec, nDuplicates);
+    logResult(1, ms, nDuplicates, nGroups, nGroupLengths);
 
     vec = orig;
-    measure("version 2", duplicateInPlace2<vec::value_type>, vec, n);
+    ms = measure(duplicateInPlace2<int>, vec, nDuplicates);
+    logResult(2, ms, nDuplicates, nGroups, nGroupLengths);
 
     vec = orig;
-    measure("version 3", duplicateInPlace3<vec::value_type>, vec, n);
+    ms = measure(duplicateInPlace3<int>, vec, nDuplicates);
+    logResult(3, ms, nDuplicates, nGroups, nGroupLengths);
 
-    vec = orig;
-    measure("version 4", duplicateInPlace4<vec::value_type>, vec, n);
+    std::printf("\n");
+}
+
+int main(int, char**)
+{
+    const std::vector<unsigned> sizes = {1, 2, 5, 10, 100, 1'000, 10'000, 100'000, 1'000'000};
+    static const unsigned finalSize = 100'000'000;
+
+    for (const auto& nDuplicates : sizes)
+        for (const auto& nGroups : sizes)
+            for (const auto& nGroupLength : sizes)
+                if (nDuplicates == 1)
+                    continue;
+                else if (nDuplicates*nGroups*nGroupLength == finalSize)
+                    measureSample(nDuplicates, nGroups, nGroupLength);
 
     return 0;
 }
