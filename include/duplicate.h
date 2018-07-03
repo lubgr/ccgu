70ef7ac40e9ef5732be6080ee69c9419ab08bcf2
Lukas Böger <dev@lboeger.de>
2018-07-04 00:02:09 +0200

    sfinae tag dispatch on equality for both versions

--- include/duplicate.h
+++ include/duplicate.h
@@ -1,34 +1,65 @@
 #ifndef DUPLICATE_H
 #define DUPLICATE_H
 
+#include <type_traits>
 #include <vector>
 
 namespace detail {
-    template<class Container> void duplicate(Container&&, Container&, unsigned) = delete;
+    template<class Container>
+        void duplicate(Container& c, Container&& result, unsigned n, std::true_type)
+        {
+            using std::begin;
+            using std::end;
+            unsigned insCount = n;
 
-    template<class Container> void duplicate(Container& c, Container&& result, unsigned n)
-    {
-        using std::begin;
-        using std::end;
-        unsigned insCount = n;
+            for (auto i = begin(c); i != end(c); ++i) {
+                auto next = i;
 
-        for (auto i = begin(c); i != end(c); ++i) {
-            auto next = i;
+                if (++next != end(c)  && *next == *i) {
+                    insCount += n;
+                    continue;
+                }
 
-            if (++next != end(c)  && *next == *i) {
-                insCount += n;
-                continue;
+                result.insert(cend(result), insCount, *i);
+                insCount = n;
             }
 
-            result.insert(cend(result), insCount, *i);
-            insCount = n;
+            c = std::move(result);
         }
 
-        c = std::move(result);
-    }
+    template<class Container>
+        void duplicate(Container& c, Container&& result, unsigned n, std::false_type)
+        {
+            using std::cend;
+
+            for (const auto& i: c)
+                result.insert(cend(result), n, i);
+
+            c = std::move(result);
+        }
+
+    template<class T, class = void>
+        struct EqualityComparable
+        : std::false_type{};
+
+    template<class...> using void_t = void;
+
+    template<class T>
+        struct EqualityComparable<T, void_t<decltype(std::declval<T&>() == std::declval<T&>())>>
+        : std::true_type{};
+
+    template<class Container>
+        void duplicate(Container& c, Container&& result, unsigned n)
+        {
+            using std::begin;
+            EqualityComparable<decltype(*begin(c))> tag{};
+
+            duplicate(c, std::move(result), n, tag);
+        }
 }
 
-template<class T> void duplicate(std::vector<T>& v, unsigned n)
+    template<class T>
+void duplicate(std::vector<T>& v, unsigned n)
 {
     std::vector<T> result;
 
@@ -37,7 +68,8 @@ template<class T> void duplicate(std::vector<T>& v, unsigned n)
     detail::duplicate(v, std::move(result), n);
 }
 
-template<class Container> void duplicate(Container& c, unsigned n)
+    template<class Container>
+void duplicate(Container& c, unsigned n)
 {
     Container result;
 
--- tests/duplicate.cpp
+++ tests/duplicate.cpp
@@ -6,86 +6,125 @@
 #include <catch2/catch.hpp>
 #include "duplicate.h"
 
-
-namespace {
-    template<class Function, class Container>
-        void invokeAndCheck(Function&& f, const Container& expected, Container container, unsigned n)
-        {
-            using std::cbegin;
-            using std::cend;
-
-            auto orig = container;
-
-            f(orig, n);
-
-            REQUIRE(expected == orig);
-        }
-
-    template<class Container>
-        void test(const Container& expected, Container input, unsigned n)
-        {
-            invokeAndCheck<void(Container&, unsigned), Container>(duplicate, expected, input, n);
-        }
-}
-
 TEST_CASE("testDuplicateVector5x2")
 {
     std::vector<int> ints{1, 3, 5, 5, 6};
-    std::vector<int> expected{1, 1, 3, 3, 5, 5, 5, 5, 6, 6};
+    const std::vector<int> expected{1, 1, 3, 3, 5, 5, 5, 5, 6, 6};
 
-    test(expected, ints, 2);
+    duplicate(ints, 2);
+
+    REQUIRE(expected == ints);
 }
 
 TEST_CASE("testDuplicateVector3x3")
 {
     std::vector<int> ints{2, 4, 7};
-    std::vector<int> expected{2, 2, 2, 4, 4, 4, 7, 7, 7};
+    const std::vector<int> expected{2, 2, 2, 4, 4, 4, 7, 7, 7};
 
-    test(expected, ints, 3);
+    duplicate(ints, 3);
+
+    REQUIRE(expected == ints);
 }
 
 TEST_CASE("testDuplicateVector2x5")
 {
     std::vector<int> ints{1, 2};
-    std::vector<int> expected{1, 1, 1, 1, 1, 2, 2, 2, 2, 2};
+    const std::vector<int> expected{1, 1, 1, 1, 1, 2, 2, 2, 2, 2};
 
-    test(expected, ints, 5);
+    duplicate(ints, 5);
+
+    REQUIRE(expected == ints);
 }
 
 TEST_CASE("testDuplicateList5x3")
 {
     std::list<int> ints{1, 1, 4, 5, 8};
-    std::list<int> expected{1, 1, 1, 1, 1, 1, 4, 4, 4, 5, 5, 5, 8, 8, 8};
+    const std::list<int> expected{1, 1, 1, 1, 1, 1, 4, 4, 4, 5, 5, 5, 8, 8, 8};
 
-    test(expected, ints, 3);
+    duplicate(ints, 3);
+
+    REQUIRE(expected == ints);
 }
 
 TEST_CASE("testDuplicateList6x4")
 {
     std::list<int> ints{0, 1, 2, 3, 4};
-    std::list<int> expected{0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
+    const std::list<int> expected{0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
 
-    test(expected, ints, 4);
+    duplicate(ints, 4);
+
+    REQUIRE(expected == ints);
 }
 
 TEST_CASE("testDuplicateDeque3x1")
 {
     std::deque<int> ints{1, 10, 100};
+    const auto expected = ints;
 
-    test(ints, ints, 1);
+    duplicate(ints, 1);
+
+    REQUIRE(expected == ints);
 }
 
 struct A { int i; };
 
 bool operator == (const A& lhs, const A& rhs)
 {
-   return lhs.i == rhs.i;
+    return lhs.i == rhs.i;
 }
 
 TEST_CASE("testDuplicateDequeUnsorted5x3")
 {
-   std::deque<A> input{{10}, {6}, {1}, {3}, {2}};
-   std::deque<A> expected{{10}, {10}, {10}, {6}, {6}, {6}, {1}, {1}, {1}, {3}, {3}, {3}, {2}, {2}, {2}};
+   std::deque<A> comparableStructs{{10}, {6}, {1}, {3}, {2}};
+   const std::deque<A> expected{{10}, {10}, {10}, {6}, {6}, {6}, {1}, {1}, {1}, {3}, {3}, {3}, {2}, {2}, {2}};
 
-   test(expected, input, 3);
+   duplicate(comparableStructs, 3);
+
+   REQUIRE(expected == comparableStructs);
+}
+
+struct B { int i; };
+
+TEST_CASE("testDuplicateVectorUnsortedNonComparable2x2")
+{
+    std::vector<B> nonComparableStructs{{20}, {10}};
+    std::vector<B> expected{{20}, {20}, {10}, {10}};
+
+    duplicate(nonComparableStructs, 2);
+
+    REQUIRE(expected.size() == nonComparableStructs.size());
+
+    for (std::vector<B>::size_type i = 0; i < expected.size(); ++i) {
+        REQUIRE(expected[i].i == nonComparableStructs[i].i);
+    }
+}
+
+struct C { int i; };
+
+bool operator == (C&& lhs, C&& rhs)
+{
+    return lhs.i == rhs.i;
+}
+
+TEST_CASE("testDuplicateDequeOnlyRvalueComparable6x2")
+{
+    std::deque<C> rvalueComparableStructs{{-10}, {-5}, {0}, {5}, {20}, {40}};
+    std::deque<C> expected{{-10}, {-10}, {-5}, {-5}, {0}, {0}, {5}, {5}, {20}, {20}, {40}, {40}};
+
+    duplicate(rvalueComparableStructs, 2);
+
+    REQUIRE(expected.size() == rvalueComparableStructs.size());
+
+    for (std::vector<B>::size_type i = 0; i < expected.size(); ++i) {
+        REQUIRE(expected[i].i == rvalueComparableStructs[i].i);
+    }
+}
+
+TEST_CASE("testZeroDuplicates")
+{
+    std::vector<unsigned> v{0, 1, 2};
+
+    duplicate(v, 0);
+
+    REQUIRE(v.empty());
 }
